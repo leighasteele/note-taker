5cf4657772e77070e3dfb5838ac3aa010ab4469f
Bambi <leighasteele10@gmail.com>
2022-09-20 13:12:13 -0400

    save

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index');
+const fs = require('fs');
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+
 
 app.use(express.static('public'));
 
