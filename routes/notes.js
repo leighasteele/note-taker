ffdfdfabe4d0abb8a1a272aea71b6e0de3790d7b
Bambi <leighasteele10@gmail.com>
2022-09-21 15:31:25 -0400

    save

---
+++ routes/notes.js
@@ -0,0 +1,15 @@
+const notes = require("express").Router();
+
+notes.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+  }
+});
